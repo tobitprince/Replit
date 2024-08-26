49c7d1456d217cc3a61bc6a9a91f038be9dc5bde
tobitprince <prince.tobit@strathmore.edu>
2024-08-26 17:58:13 +0300

    feat: Add user authentication functionality
    The code changes introduce user authentication functionality, allowing users to enter their username and password. If the credentials match, the user is greeted with a personalized welcome message. Otherwise, an error message is displayed indicating incorrect username or password.

---
+++ 100 days of code/day6.py
@@ -0,0 +1,7 @@
+print("Welcome ")
+username = input("Please enter you username: ")
+password = input("Please enter you password: ")
+if username == "jack" and password == "jack":
+  print("Welcome Jack")
+else:
+  print("Wrong Username or Password")
