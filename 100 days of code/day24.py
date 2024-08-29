5b0fb81d95743f46d92c9d6e8e57d9b336521814
tobitprince <prince.tobit@strathmore.edu>
2024-08-29 11:22:02 +0300

    chore: Add Python scripts for user input and output

---
+++ 100 days of code/day24.py
@@ -0,0 +1,19 @@
+def rollDice(sides):
+  import random
+  roll = random.randint(1,sides)
+  print(f"You rolled {roll}")
+  
+
+print("Infinity Dice")
+print()
+print()
+sides = int(input("How many sides?: "))
+while True:
+  rollDice(sides)
+  answer = input("Roll again?: ").lower()
+
+  if answer == "yes":
+    continue
+  else:
+    break
+print("Goodbye!")
---
+++ 100 days of code/day25.py
@@ -0,0 +1,20 @@
+def anyDice(sides):
+  import random
+  result= random.randint(1,sides)
+  return result
+
+def dice6_8():
+  six = anyDice(6)
+  eight = anyDice(8)
+
+  result = six * eight
+  return result
+
+print("Character Health Stats")
+print()
+character = "yes"
+
+while character == "yes":
+  health = dice6_8()
+  print(f"Character health stat is {health} hp")
+  character = input("Generate for another character?: ")
