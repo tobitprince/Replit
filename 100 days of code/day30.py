12ee22d9432eb0bac3bbe3423fd56fbef329baa8
tobitprince <prince.tobit@strathmore.edu>
2024-09-01 22:29:14 +0300

    feat: Add color printing functionality
    The code changes introduce a new function `newPrint` that allows printing text in different colors. The function takes a color parameter and a word parameter, and prints the word in the specified color. Additionally, the code includes an example usage of the `newPrint` function to print text in red and blue colors.

---
+++ 100 days of code/day30.py
@@ -0,0 +1,9 @@
+print()
+print("30 Days Down".center(35))
+print("....................................")
+
+for i in range(1,31):
+  print()
+  answer = input(f"How was Day {i}? : ")
+  print()
+  print(f"You thought Day {i} was {answer}".center(35))
