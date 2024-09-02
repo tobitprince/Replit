aec10fc9e448726d59cf7a5bdafe71eabee572b6
tobitprince <prince.tobit@strathmore.edu>
2024-09-02 16:36:42 +0300

    chore: Update determine_generation function to use if-else statements
    Refactor the determine_generation function in day9.py to use if-else statements instead of multiple if statements. This improves code readability and simplifies the logic for determining the generation based on the year of birth.

--- 100_days_of_code/day9.py
+++ 100_days_of_code/day9.py
@@ -8,16 +8,15 @@ def determine_generation(year_of_birth):
     """
     if 1925 <= year_of_birth <= 1946:
         return "You are a Traditionalist."
-    elif 1947 <= year_of_birth <= 1964:
+    if 1947 <= year_of_birth <= 1964:
         return "You are a Baby Boomer."
-    elif 1965 <= year_of_birth <= 1981:
+    if 1965 <= year_of_birth <= 1981:
         return "You are a Generation X"
-    elif 1982 <= year_of_birth <= 1995:
+    if 1982 <= year_of_birth <= 1995:
         return "You are a Millenial"
-    elif 1996 <= year_of_birth <= 2015:
+    if 1996 <= year_of_birth <= 2015:
         return "You are a Generation Z"
-    else:
-        return "You are a Generation Alpha"
+    return "You are a Generation Alpha"
 
 print()
 year_of_birth1 = int(input("What year were you born? "))
