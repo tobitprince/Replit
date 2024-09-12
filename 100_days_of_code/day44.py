6b07ad18dc3acc5e0c12ac8499dbde2b1eda374b
tobitprince <prince.tobit@strathmore.edu>
2024-09-12 23:15:35 +0300

    Day44

---
+++ 100_days_of_code/day44.py
@@ -0,0 +1,41 @@
+"""
+2D Dynamic
+"""
+
+
+def prettyprint():
+    """_summary_
+    """
+    for row in lists2d:
+        for item in row: 
+            print(f"{item:^10}", end=" | ")
+        print()
+    print()
+
+lists2d = []
+
+while True:
+    menu = input("Add or remove: ")
+    if menu.strip().lower()[0] == "a":
+        name = input("what is your name: ")
+        age = int(input("What is your age: "))
+        weight = int(input("What is your weight in kilos: "))
+
+        new_row = [name, age, weight]
+
+        lists2d.append(new_row)
+        prettyprint()
+
+    else:
+        name = input("What do you want to delete? ")
+        for row in lists2d[:]:
+            if name in row:
+                lists2d.remove(row)
+        prettyprint()
+
+    terminate = input("Want to exit? y/n: ")
+
+    if terminate.strip().lower()[0] == "y":
+        break
+
+prettyprint()
