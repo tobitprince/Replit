7d7c90f36c03e5d8e084ff213cd4b9592c3ff1af
tobitprince <prince.tobit@strathmore.edu>
2024-11-04 22:29:21 +0300

    Day 61

--- 100_days_of_code/day61.py
+++ 100_days_of_code/day61.py
@@ -1,3 +1,30 @@
-from replit import db
+import os
+import time
+
+def add():
+    """_summary_
+    """
+    tweet = input()
+
+def view():
+    """_summary_
+    """
+    tweet = input()
+
+def main():
+    """_summary_
+    """
+    while True:
+        os.system("cls")
+        print("Tweeter".center(35,"*"))
+        menu = int(input("1: Add Tweet\n2: View Tweet"))
+        if menu == 1:
+            add()
+        if menu == 2:
+            view()
+
+if __name__ == "__main__":
+    main()
+
 
 
